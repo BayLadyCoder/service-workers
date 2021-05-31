6418692919459209fb92882e7c90acab55112211
BayLadyCoder <rachada_32@hotmail.com>
2021-05-30 21:33:28 -0400

    Added cacheAssets (cache files) and cacheName (cache version) to CacheStorage on Service Worker Install event

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,6 +1,24 @@
+const cacheName = "v1";
+
+const cacheAssets = [
+  "index.html",
+  "about.html",
+  "/css/styles.css",
+  "/js/app.js",
+];
+
 // Call Install Event
 self.addEventListener("install", (e) => {
   console.log("Service Worker: Installed");
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Service Worker: Caching Files");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
 // Call Activate Event
