7eeaf105bd7ca1b5c38d5114dbf7e07fe18c6e68
BayLadyCoder <rachada_32@hotmail.com>
2021-05-30 21:26:51 -0400

    AddEventListener to install and activate events

---
+++ sw_cached_pages.js
@@ -0,0 +1,9 @@
+// Call Install Event
+self.addEventListener("install", (e) => {
+  console.log("Service Worker: Installed");
+});
+
+// Call Activate Event
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker: Activated");
+});
